0450e23ce08543bfec71cac2bd9ed190cbbfd1b3
saadsgh123 <sdsghouri@gmail.com>
2023-09-13 00:53:48 +0100

    task 8 try 5

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,12 +16,15 @@ void jack_bauer(void)
 {
 	for (m2 = 0; m2 < 10; m2++)
 {
+	if ((h1 <= 2 && h2 <= 10) || (h1 <= 2 && h2 <= 3))
+{
 		_putchar(h1 + '0');
 		_putchar(h2 + '0');
 		_putchar(':');
 		_putchar(m1 + '0');
 		_putchar(m2 + '0');
 		_putchar('\n');
+}
 }
 }
 }
