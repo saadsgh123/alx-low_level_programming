bc3531c69a699976ff9afe7cdda8a0a18faf086a
saadsgh123 <sdsghouri@gmail.com>
2023-09-13 00:28:23 +0100

    task 8 try

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,10 +3,10 @@
  * starting from 00:00 to 23:59.
  * Return: Always void.
  */
-include "main.h"
+#include "main.h"
 void jack_bauer(void)
 {
-	int h1,h2,m1,m2;
+	int h1, h2, m1, m2;
 
 	for (h1 = 0 ; h1 < 3; h1++)
 {
@@ -16,10 +16,10 @@ void jack_bauer(void)
 {
 	for (m2 = 0; m2 < 10; m2++)
 {
-		_putchar(h1+'0');
-		_putchar(h2+'0');
-		_putchar(m1+'0');
-		_putchar(m2+'0');
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
 		_putchar('\n');
 }
 }
