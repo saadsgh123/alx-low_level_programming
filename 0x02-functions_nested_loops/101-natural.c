2b37df0897c227eb59157244b635e90575cf597f
saadsgh123 <sdsghouri@gmail.com>
2023-09-13 16:20:58 +0100

    swddwdwe

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,33 @@
 /**
- * main -  that prints all natural numbers from n to 98
+ * multiple -  that prints all natural numbers from n to 98
+ * main - print the multiple below 1024
  * followed by a new line.
  * Return: Always void.
  */
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-void multiple()
+void multiple(void)
 {
 int x;
-int i=0;
+int i = 0;
 
 for (x = 1023; x > 0; x--)
 {
 if (x % 3 == 0 || x % 5 == 0)
 {
-i += x;
+i + = x;
 }
 }
-printf("%d\n",i);
+printf("%d\n", i);
 }
-
-int main()
+/**
+ * main - print the multiple below 1024
+ * followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
 {
-    multiple();
-    return (0);
+multiple();
+return (0);
 }
