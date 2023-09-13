438f83ddd522923f1cb4130cdcf75f2995e9524f
saadsgh123 <sdsghouri@gmail.com>
2023-09-13 20:49:34 +0100

    deokdowekdwe

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+/**
+ * main -  that prints all natural numbers from n to 98
+ * followed by a new line.
+ * Return: Always void.
+ */
+#include <stdio.h>
+int main(void)
+{
+unsigned long long int fib1 = 1, fib2 = 2;
+int count = 2;
+printf("%lu, %lu, ", fib1, fib2);
+while (count < 50)
+{
+unsigned long nextFib = fib1 + fib2;
+printf("%lu", nextFib);
+if (count < 49)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+fib1 = fib2;
+fib2 = nextFib;
+count++;
+}
+return (0);
+}
