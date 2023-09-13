3c7857c0ac9a6205dcf5f6db55da1659e4e5d0f8
saadsgh123 <sdsghouri@gmail.com>
2023-09-13 16:13:49 +0100

    cjidjcd

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+/**
+ * main -  that prints all natural numbers from n to 98
+ * followed by a new line.
+ * Return: Always void.
+ */
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+void multiple()
+{
+int x;
+int i=0;
+
+for (x = 1023; x > 0; x--)
+{
+if (x % 3 == 0 || x % 5 == 0)
+{
+i += x;
+}
+}
+printf("%d",i);
+}
+
+int main()
+{
+    multiple();
+    return (0);
+}
