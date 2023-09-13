7ff98ca2084d31baa85b3b8d5ccbc6b70b2875b1
saadsgh123 <sdsghouri@gmail.com>
2023-09-13 23:44:05 +0100

    task 3 try 5

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-the entire header file file
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+
 #endif
