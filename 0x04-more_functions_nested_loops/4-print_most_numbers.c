8b591346c39c7906162120200ea1f0999f8151b0
saadsgh123 <sdsghouri@gmail.com>
2023-09-14 14:36:54 +0100

    task 5 try 1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+/**
+* print_most_numbers - function that prints the numbers, from 0 to 9
+* followed by a new line.
+* Return: void.
+*/
+#include "main.h"
+void print_most_numbers(void)
+{
+int i=0;
+while (i < 10){
+if (i != 2 & i!= 4)
+{
+printf("%d", i);
+}
+i++;
+}
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void _putchar(char a);
+void print_most_numbers(void);
 
 #endif
