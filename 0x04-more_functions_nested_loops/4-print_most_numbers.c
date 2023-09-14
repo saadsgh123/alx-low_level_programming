16321ada19954fcf1df76f6c1a12125158d957a4
saadsgh123 <sdsghouri@gmail.com>
2023-09-14 15:54:45 +0100

    edjiejdiee

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 int i = 0;
 while (i < 10)
 {
-if (i != 2 || i != 4)
+if (i != 2 & i != 4)
 {
-continue;
+putchar(i + '0');
 }
-_putchar(i + '0');
 i++;
 }
+putchar('\n');
 }
