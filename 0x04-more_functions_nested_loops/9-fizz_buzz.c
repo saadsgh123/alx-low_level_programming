af3917f49d2e16d160067cc4292df328c95ad2a7
saadsgh123 <sdsghouri@gmail.com>
2023-09-14 23:23:49 +0100

    helllo saaad

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+* main - Fizzbuzz
+* followed by new line.
+* Return:  (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 1; i < 100; i++)
+{
+if (i % 5 == 0 & i % 3 == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+printf(" ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else
+{
+printf("%d", i);
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
