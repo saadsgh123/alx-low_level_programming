e879233edf2cd55c8c282f24cc9b57bbb51b5787
saadsgh123 <sdsghouri@gmail.com>
2023-09-15 01:47:10 +0100

    dewkdokeow

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,7 @@
 #include "main.h"
 void print_number(int n)
 {
+
 if (n < 0)
 {
 _putchar('-');
@@ -18,5 +19,5 @@ if (n / 10 != 0)
 print_number(n / 10);
 }
 
-_putchar('0' + n % 10);
+_putchar(n % 10 + '0');
 }
