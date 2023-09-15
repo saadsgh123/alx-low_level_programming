4c2b315b5b636abdd6b133f2518062688f6d279d
saadsgh123 <sdsghouri@gmail.com>
2023-09-15 01:31:05 +0100

    safiplz

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+/**
+ * print_number - prints integers
+ * @n: Accepts integer
+ *
+ * Return: Nothing (always)
+ */
+#include "main.h"
+void print_number(int n)
+{
+if (n < 10)
+{
+_putchar(n + '0');
+}
+else if (n < 100)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if (n < 1000)
+{
+_putchar((n / 100) + '0');
+_putchar(((n / 10) % 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if (n >= 1000)
+{
+_putchar((n / 1000) + '0');
+_putchar(((n / 100) % 10) + '0');
+_putchar(((n / 10) % 10) + '0');
+_putchar((n % 10) + '0');
+}
+}
