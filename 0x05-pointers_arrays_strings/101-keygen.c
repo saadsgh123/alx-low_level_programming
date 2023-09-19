e0cb9896c10ce271798fb135f4c3a71a7651d4b2
saadsgh123 <sdsghouri@gmail.com>
2023-09-19 02:45:14 +0100

    task 101 try 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,27 +10,26 @@
  */
 int main(void)
 {
-int pass[100];
-int i, sum, n;
+	int pass[100];
+	int i, sum, n;
 
-sum = 0;
+	sum = 0;	
 
-srand(time(NULL));
+	srand(time(NULL));
 
-for (i = 0; i < 100; i++)
-{
-pass[i] = rand() % 78;
-sum += pass[i] + 33;
-putchar(pass[i] + 33);
-if (sum >= 2772)
-break;
-}
-n = 2772 - sum;
-while (n > 0)
-{
-char valid_char = rand() % 78 + 33;
-putchar(valid_char);
-n -= valid_char - 33;
-}
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
 }
