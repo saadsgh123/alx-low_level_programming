1bf61d9ed11b7d191823b7da84f3dda652ca917c
saadsgh123 <sdsghouri@gmail.com>
2023-09-19 01:15:51 +0100

    task 9 tr< 1

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_array - prints every other character of a string
+*
+* @a: list of character.
+* @n: number of elements.
+* Return: void (Success)
+*/
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; ++i)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+{
+printf(", ");
+}
+}
+_putchar('\n');
+}
