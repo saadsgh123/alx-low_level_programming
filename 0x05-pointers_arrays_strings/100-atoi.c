3fca6e91bffb7fba935d29474d55fdbd4d18c3de
saadsgh123 <sdsghouri@gmail.com>
2023-09-19 02:30:34 +0100

    task 11 try 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* _atoi - prints every other character of a string
+*
+* @str: list of character.
+*
+* Return: result (Success)
+*/
+int _atoi(char *str)
+{
+int result = 0;
+int sign = 1;
+
+while (*str == ' ' || *str == '\t' || *str == '\n')
+{
+str++;
+}
+if (*str == '-')
+{
+sign = -1;
+str++;
+}
+else if (*str == '+')
+{
+str++;
+}
+while (*str >= '0' && *str <= '9')
+{
+result = result * 10 + (*str - '0');
+str++;
+}
+return (result *sign);
+}
