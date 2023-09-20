ad1befca913026ea5cb32fb4253c9d0d69efd806
saadsgh123 <sdsghouri@gmail.com>
2023-09-20 21:03:23 +0100

    task 4 try 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,25 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0' || *s2 != '\0')
+int i=0;
+while (*s1 != '\0' && *s2 != '\0')
 {
 if (*s1 != *s2)
 {
-return ((*s1 > *s2) ? (15) : (-15));
+return (*s1 - *s2);
 }
 s1++;
 s2++;
+i++;
 }
-if (*s1 == '\0' && *s2 == '\0')
-{
 return (0);
-}
-else if (*s1 == '\0')
-{
-return (-15);
-}
-else
-{
-return (15);
-}
 }
