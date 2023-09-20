7d1f342a85375997c82800279732e31d2999aaf3
saadsgh123 <sdsghouri@gmail.com>
2023-09-20 19:33:18 +0100

    task 2 try 4

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,17 +6,19 @@
  * @n: number of bytes.
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, const char *src, int n)
 {
 int i;
-
-for (i = 0; i < n; ++i)
+    
+for (i = 0; i < n && src[i] != '\0'; ++i)
 {
 dest[i] = src[i];
 }
+
 for (; i < n; ++i)
 {
 dest[i] = '\0';
 }
+
 return (dest);
 }
