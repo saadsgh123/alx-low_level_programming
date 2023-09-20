5d3dffdfc9a0cb02794b4b5b5c37baa46a0df80a
saadsgh123 <sdsghouri@gmail.com>
2023-09-20 19:21:54 +0100

    task 2 try 1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string.
+ * @dest: destination.
+ * @src: source.
+ * @n: number of bytes.
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n; ++i)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
+/**
+ * length - function that calculate string length
+ * @s: string.
+ * Return: len
+ */
+int length(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+s++;
+len++;
+}
+return (len);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ void _putchar(char a);
 char *_strcat(char *dest, char *src);
 int length(char *s);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
