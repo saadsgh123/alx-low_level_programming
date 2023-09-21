e76651b687c401a30ab503d0a7ddfff896837760
saadsgh123 <sdsghouri@gmail.com>
2023-09-21 19:03:19 +0100

    task 10 try 3

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,14 +5,15 @@
  */
 void print_number(int n)
 {
+unsigned int new = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+new = -n;
 }
-if (n / 10 != 0)
+if (new / 10 != 0)
 {
-print_number(n / 10);
+print_number(new / 10);
 }
 _putchar(n % 10 + '0');
 }
