ef4e819a1cfe84731a61420d484ad05d9501c466
saadsgh123 <sdsghouri@gmail.com>
2023-09-21 13:48:45 +0100

    task 6 try 1

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+char *original = n;
+while (*n != '\0')
+{
+if (*n >= 97 & *n <= 122)
+{
+*n = *n - 32;
+}
+n++;
+}
+return (original);
+}
