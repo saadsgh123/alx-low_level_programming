d95c42e060340e4fadb962e73c50c6a29e602f0d
saadsgh123 <sdsghouri@gmail.com>
2023-09-25 18:40:48 +0100

    task 3 try done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,30 @@
 #include "main.h"
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: string to check.
+ * @accept: prefix substring
+ * Return: number of bytes
  */
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int n = 0;
-	int r;
+ unsigned int _strspn(char *s, char *accept)
+ {
+unsigned int size = 0;
+int i;
 
-	while (*s)
-	{
-		for (r = 0; accept[r]; r++)
-		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
-		}
-		s++;
-	}
-	return (n);
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+size++;
+break;
+}
+else if (!accept[r + 1])
+{
+return (n);
+}
+}
+s++;
+}
+return (n);
 }
