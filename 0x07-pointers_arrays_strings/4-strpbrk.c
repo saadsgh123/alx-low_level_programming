415ccc1399c22f9d323b0675313f495bee6e2844
saadsgh123 <sdsghouri@gmail.com>
2023-09-25 20:08:03 +0100

    task 4 try 3

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,9 +9,9 @@ char *_strpbrk(char *s, char *accept)
 {
 int i, j;
 
-for (i = 0; s[i]; ++i)
+for (i = 0; s[i]; i++)
 {
-for (j = 0; accept[j]; ++j)
+for (j = 0; accept[j]; j++)
 {
 if (s[i] == accept[j])
 {
