c2acc93ebb28545a58914e1e59eb15377437051e
saadsgh123 <sdsghouri@gmail.com>
2023-09-25 20:02:52 +0100

    task 4 try 1

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - function that gets the length of a prefix substring.
+ * @s: string to check.
+ * @accept: prefix substring
+ * Return: number of bytes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+for (i = 0; s[i]; ++i)
+{
+for (j = 0; accept[j]; ++j)
+{
+if (s[i] == accept[j])
+{
+return (s + i);
+}
+}
+}
+return NULL;
+}
