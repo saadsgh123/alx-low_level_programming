3e53f0cdfc301b64adcb69cc997d51f386e6f889
saadsgh123 <sdsghouri@gmail.com>
2023-10-02 21:56:46 +0100

    task 1 try 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point.
+ *
+ * @argc: size of arguments
+ * @argv: arguments array
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+printf("%d\n",argc);
+return (0);
+}
