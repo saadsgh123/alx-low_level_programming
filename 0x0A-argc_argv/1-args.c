aeade5b0b713c8d687a51dc3082891ce08b8957f
saadsgh123 <sdsghouri@gmail.com>
2023-10-02 13:18:09 -0700

    hello wolld

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: size of arguments
+ * @argv: arguments array
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s",argv[0]);
+return (0);
+}
