a2fb2c14eb38abd44323a84e9b0542c5b716dc25
saadsgh123 <sdsghouri@gmail.com>
2023-10-03 13:46:44 +0100

    task 3 try 1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: size of arguments
+ * @argv: arguments array
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int res;
+if (argc <= 1)
+{
+printf("Error");
+}
+
+if (argc >= 2)
+{
+res = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", res);
+}
+
+return (0);
+}
