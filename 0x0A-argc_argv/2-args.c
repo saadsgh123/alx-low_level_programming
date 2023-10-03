3b28f1ca6087e82f20f2d4ff3e035ae4c273792f
saadsgh123 <sdsghouri@gmail.com>
2023-10-03 13:36:14 +0100

    task 2 try 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point.
+ *
+ * @argc: size of arguments
+ * @argv: arguments array
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+return (0);
+}
