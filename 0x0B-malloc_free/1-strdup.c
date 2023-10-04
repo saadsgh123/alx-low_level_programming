8eb726ef2cbd3ca257756928e6ee56f4163b156e
saadsgh123 <sdsghouri@gmail.com>
2023-10-04 15:51:06 +0100

    task 1 try 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - prints buffer in hexa
+ * @str: the size of the memory to print
+ *
+ * Return: 0.
+ */
+char *_strdup(char *str)
+{
+int i;
+int size = str_leng(str, 0);
+
+if (str == NULL)
+return (NULL);
+
+char *new = malloc(sizeof(char) * size);
+
+for (i = 0; i < size; ++i)
+{
+new[i] = str[i];
+}
+return (new);
+}
+/**
+ * str_leng - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+int str_leng(char *c, int size)
+{
+while (*c)
+{
+return (str_leng(c + 1, size + 1));
+}
+return (size);
+}
