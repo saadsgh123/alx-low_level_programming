90105eb92274dcb77abfdfe1468efc8be72d59cf
saadsgh123 <sdsghouri@gmail.com>
2023-10-04 20:11:22 +0100

    task 2 try 1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+int lengt(char *s);
+/**
+ * str_concat - prints buffer in hexa
+ *
+ * @s1: the first string.
+ * @s2: the second string.
+ *
+ * Return: Nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+int i = 0, j, size1, size2;
+char *new;
+
+if (s1 == NULL || s2 == NULL)
+new = "";
+
+size1 = lengt(s1);
+size2 = lengt(s2);
+
+new = malloc(sizeof(char) * (size1 + size2 + 1));
+
+if (new == NULL)
+return (NULL);
+
+for (; s1[i] != '\0'; ++i)
+{
+new[i] = s1[i];
+}
+for (j = 0; s2[j] != '\0'; ++j)
+{
+new[i + j] = s2[j];
+}
+new[i + j] = '\0';
+
+return (new);
+}
+/**
+ * lengt - prints buffer in hexa
+ *
+ * @s: the first string.
+ *
+ * Return: Nothing.
+ */
+int lengt(char *s)
+{
+
+int size = 0;
+
+while (s[size] != '\0')
+size++;
+
+return (size);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int str_leng(char *c,int size);
+char *str_concat(char *s1, char *s2);
 
 #endif
