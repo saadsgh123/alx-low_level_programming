8ed54e989106ae54c843db04fb1ebf90c9d3454f
saadsgh123 <sdsghouri@gmail.com>
2023-10-05 20:49:49 +0100

    dwefefwf

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-int _is_alpha(char c);
 /**
- * main - Entry point.
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
  *
- * @argc: size of arguments
- * @argv: arguments array
- *
- * Return: 0.
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int i, x, sum = 0;
-
+int i = 1;
+int sum = 0;
 if (argc == 1)
 {
 printf("%d\n", 0);
 }
+else if (argc > 2)
+{
+for (; i < argc; i++)
+{
+if (atoi(argv[i]))
+{
+sum += atoi(argv[i]);
+}
 else
 {
-for (i = 1; i < argc; i++)
-{
-x = atoi(argv[i]);
-if (_is_alpha(argv[i][0]))
-{
-printf("Error\n");
+printf("%s\n", "Error");
 return (1);
 }
-else
-{
-sum = sum + x;
 }
-}
-if (sum > 0)
-{
 printf("%d\n", sum);
-}
-}
-return (0);
-}
-/**
- * _is_alpha - function that check if it is an alphabet.
- *
- * @c: character to check
- *
- * Return: 0.
- */
-int _is_alpha(char c)
-{
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-return (1);
 }
 return (0);
 }
