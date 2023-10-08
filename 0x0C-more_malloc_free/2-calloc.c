8d5b9671adb5306f1a8c081163635d3e8582cd4a
saadsgh123 <sdsghouri@gmail.com>
2023-10-08 11:44:44 +0100

    tiejdiejde

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+/**
+ * _calloc - check the code
+ * @nmemb: number of elements
+ * @size: size of each element.
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+void *p;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+p = malloc(10 * size);
+
+if (p == NULL)
+return (NULL);
+
+return (p);
+}
