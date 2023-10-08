614b7546e8501d35fc4a07426665e204783aa3da
saadsgh123 <sdsghouri@gmail.com>
2023-10-08 11:59:00 +0100

    ieidiiidid+

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,6 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int length(char *s);
-*_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
