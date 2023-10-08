711dbb1f4c6631201f80cc995faa64871060a937
saadsgh123 <sdsghouri@gmail.com>
2023-10-08 23:38:30 +0100

    task 5 done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int *array_range(int min, int max);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-int length(char *s);
-char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
