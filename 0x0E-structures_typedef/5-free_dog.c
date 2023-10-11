98e655e13e2707282cde2f8c01c10542f862f74f
saadsgh123 <sdsghouri@gmail.com>
2023-10-11 13:46:02 +0100

    jdejdeijd

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dog.h>
+/**
+ * free_dog - check the code
+ * @dog: pointer to dog_t.
+ */
+void free_dog(dog_t *dog)
+{
+if (dog != NULL)
+free(dog);
+}
