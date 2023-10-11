39ecc567624a2d7315751ff129f39c359dd5b853
saadsgh123 <sdsghouri@gmail.com>
2023-10-11 17:58:23 +0100

    task 1 try 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,17 +10,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
-
-	if (*array || size != 0)
-	{
-		while (i < size)
-	{
-			action(array[i]);
-			i++;
-	}
-	}
-	else
-	{
+	if (action == NULL)
 		return;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
 	}
-}
