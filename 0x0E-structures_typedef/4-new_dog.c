d6975fe0b696f99d157423c249c8fa8862d781fe
saadsgh123 <sdsghouri@gmail.com>
2023-10-11 01:32:22 +0100

    deodkeodk

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,9 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name != NULL && owner != NULL)
 	{
-		newd->name = name;
-		newd->owner = owner;
-		newd->age = age;
+		newd->name = strdup(name);
+        	newd->owner = strdup(owner);
+        	newd->age = age;
 	}
 	else
 	{
