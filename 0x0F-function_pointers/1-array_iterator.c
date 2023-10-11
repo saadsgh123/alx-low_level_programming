c95d8c8dc7b4ba3face994c01637c37e1bb7f22b
saadsgh123 <sdsghouri@gmail.com>
2023-10-11 17:54:09 +0100

    task 1 try 0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - print name using pointer to function
+ * @array: string to add.
+ * @size: pointer to function.
+ * @action: action pointer.
+ * Return: nothing
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (*array || size != 0)
+	{
+		while (i < size)
+	{
+			action(array[i]);
+			i++;
+	}
+	}
+	else
+	{
+		return;
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
 #endif
