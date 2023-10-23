0bccf0fdd7b1f2f96a333c6120250fad426bd227
saadsgh123 <sdsghouri@gmail.com>
2023-10-23 17:27:13 +0100

    task 5 done

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -13,6 +13,7 @@ void free_listint(listint_t *head)
 	while (head != NULL)
 	{
 		listint_t *temp = head;
+
 		head = head->next;
 		free(temp);
 	}
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * free_listint2 - returns the number of elements in a linked lists
+ * @head: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
@@ -13,9 +13,11 @@ void free_listint2(listint_t **head)
 	}
 
 	listint_t *current = *head;
+
 	while (current != NULL)
 	{
 		listint_t *temp = current;
+
 		current = current->next;
 		free(temp);
 	}
