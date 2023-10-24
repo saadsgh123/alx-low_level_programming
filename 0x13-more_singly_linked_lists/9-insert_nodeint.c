07ae6223b723e26fc09b0d62ecdb2575f28fb05b
saadsgh123 <sdsghouri@gmail.com>
2023-10-24 23:38:56 +0100

    task 9 try 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,5 +25,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp->next = ptr->next;
 	ptr->next = temp;
 
-	return ((*head) ? (*head) : NULL);
+	return ((*head));
 }
