5025219060c2468530cb3f996b8f81da0696dcb6
saadsgh123 <sdsghouri@gmail.com>
2023-10-25 19:12:33 +0100

    task 12 try 1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,11 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+typedef struct listp_s
+{
+    void *p;
+    struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -28,5 +33,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-
+size_t print_listint_safe(const listint_t *head);
+void free_listp(listp_t **head);
 #endif
