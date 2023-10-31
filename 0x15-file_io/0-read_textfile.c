25594a20ceee9ddc33d2407bda0e736e3b5a0713
saadsgh123 <sdsghouri@gmail.com>
2023-10-31 19:03:35 +0100

    task 0 try 4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,8 +8,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	int file_descriptor, i = 0;
-	char buffer[letters];
+	int file_descriptor;
+	char *buffer;
 	ssize_t bytes_read, nwr;
 
 	file_descriptor = open(filename, O_RDONLY);
@@ -18,12 +18,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
+    
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+		return (0);
 
 	bytes_read = read(file_descriptor, buffer, sizeof(buffer));
 
 	nwr = write(STDOUT_FILENO, buffer, bytes_read);
 
+
 	close(file_descriptor);
+    free(buffer);
 
 	return (nwr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/fcntl.h>
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
