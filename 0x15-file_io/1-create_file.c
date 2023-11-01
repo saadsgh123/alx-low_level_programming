a354e43f1c46bd3d75364ad5297539883663f207
saadsgh123 <sdsghouri@gmail.com>
2023-11-01 23:22:16 +0100

    task 1 try 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_RDWR);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (file_descriptor < 0)
 		return (-1);
@@ -25,7 +25,6 @@ int create_file(const char *filename, char *text_content)
 
 	while (text_content[i])
 	{
-		text_content++;
 		i++;
 	}
 	n_out = write(file_descriptor, text_content, i);
