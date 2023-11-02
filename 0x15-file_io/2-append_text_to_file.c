3f8194ddeb3704ea14a5b1af194966a8671ef889
saadsgh123 <sdsghouri@gmail.com>
2023-11-02 20:59:05 +0100

    task 2 try 4

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,18 +12,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file_descriptor, i = 0;
 	int n_out;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
 	if (file_descriptor < 0)
 	{
 		return (-1);
 	}
-	while (text_content[i])
-		i++;
-	if (text_content != NULL)
+
+	if (text_content)
 	{
+		while (text_content[i])
+			i++;
+
 		n_out = write(file_descriptor, text_content, i);
 		if (n_out == -1)
 		{
