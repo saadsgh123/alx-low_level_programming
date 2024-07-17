b4265e1be00c9660c203f7c636293bd27649b685
saadsgh123 <sdsghouri@gmail.com>
2024-07-17 21:04:26 +0100

    dijwiedjowijd

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,7 +10,6 @@
   * Return: If the value is not present or the array is NULL, -1.
   *         Otherwise, the index where the value is located.
   *
-  * Description: Prints the [sub]array being searched after each change.
   */
 int binary_search(int *array, size_t size, int value)
 {
